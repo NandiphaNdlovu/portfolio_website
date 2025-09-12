d3bb2f3d578b7244989d8576d71e7fbaaaed6c2f
NandiphaNdlovu <biameleng@icloud.com>
2025-09-12 20:45:41 +0200

    feat: timeline of achievements

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { NavBar } from './components/NavBar';
-import { Banner } from './components/Banner';
-import { Projects } from './components/Projects';
-import { Footer } from './components/Footer';
-import { Skills } from './components/Skills';
-import Particles from './components/Particles';
+import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { NavBar } from "./components/NavBar";
+import { Banner } from "./components/Banner";
+import { Projects } from "./components/Projects";
+import { Footer } from "./components/Footer";
+import { Skills } from "./components/Skills";
+import { HistoryTimeline } from "./components/HistoryTimeline/HistoryTimeline";
+import Particles from "./components/Particles";
 
 function App() {
   return (
@@ -14,7 +15,8 @@ function App() {
       <Particles />
       <NavBar />
       <Banner />
-      <Skills/>
+      <Skills />
+      <HistoryTimeline />
       <Projects />
       <Footer />
     </div>
